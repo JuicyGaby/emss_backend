60185291f11d0d58508f1e073f4c316326cd4dea
Gabe Fletcher Sanchez <gabefletchers@gmail.com>
2024-03-15 10:59:04 +0800

    tested post put get api for problems in environment

--- repository/assessment-tool.js
+++ repository/assessment-tool.js
@@ -710,6 +710,7 @@ async function getProblemsInEnvironment(patient_id) {
   }
   return response || false;
 }
+
 async function createPatientProblemsEnvironment(reqBody) {
   let problems_presented = "";
   let reasons_psychosocial_counselling = "";
@@ -764,7 +765,6 @@ async function createPatientProblemsEnvironment(reqBody) {
 async function updatePatientProblemsEnvironment(reqBody) {
   let problems_presented = "";
   let reasons_psychosocial_counselling = "";
-
   if (reqBody.problems_presented) {
     if (reqBody.problems_presented) {
       let problems = reqBody.problems_presented;
@@ -775,6 +775,45 @@ async function updatePatientProblemsEnvironment(reqBody) {
       reasons_psychosocial_counselling = reasons.join(",");
     }
   }
+  const updatedRecord = await prisma.patient_problems_environment.update({
+    where: {
+      id: reqBody.id,
+    },
+    data: {
+      patient_id: reqBody.patient_id,
+      lack_regular_food: reqBody.lack_regular_food,
+      nutritionally_inadequate_food: reqBody.nutritionally_inadequate_food,
+      documented_malnutrition: reqBody.documented_malnutrition,
+      absence_of_shelter: reqBody.absence_of_shelter,
+      inadequate_shelter: reqBody.inadequate_shelter,
+      unemployment: reqBody.unemployment,
+      underemployment: reqBody.underemployment,
+      inappropiate_employment: reqBody.inappropiate_employment,
+      insufficient_community_resources:
+        reqBody.insufficient_community_resources,
+      insufficient_provide_resources: reqBody.insufficient_provide_resources,
+      no_personal_transportation: reqBody.no_personal_transportation,
+      no_problems: reqBody.no_problems,
+      absence_of_affectional_support: reqBody.absence_of_affectional_support,
+      inadequate_support_system: reqBody.inadequate_support_system,
+      excessive_support_system: reqBody.excessive_support_system,
+      // array
+      problems_presented: problems_presented,
+      reasons_psychosocial_counselling: reasons_psychosocial_counselling,
+      // end of array
+      assesment_findings: reasons_psychosocial_counselling,
+      recommended_intervention: reqBody.recommended_intervention,
+      action_taken: reqBody.action_taken,
+      person_emergency: reqBody.person_emergency,
+      relationship_to_patient: reqBody.relationship_to_patient,
+      address: reqBody.address,
+      contact_number: reqBody.contact_number,
+      interviewed_by: reqBody.interviewed_by,
+      remarks: reqBody.remarks,
+    },
+  });
+  console.log("updated", updatedRecord);
+  return updatedRecord;
 }
 
 module.exports = {
